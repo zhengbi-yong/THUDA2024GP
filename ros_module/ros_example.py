75c6d121e8d74b77e64e8a48377a18d52a10fcaa
Kenny Shaw <kshaw2@andrew.cmu.edu>
2023-09-14 19:35:28 -0400

    Update ros_example.py

--- ros_module/ros_example.py
+++ ros_module/ros_example.py
@@ -18,11 +18,16 @@ class Telekinesis:
     def __init__(self):        
         rospy.wait_for_service('/leap_position')
         self.leap_position = rospy.ServiceProxy('/leap_position', leap_position)
+        #self.leap_velocity = rospy.ServiceProxy('/leap_velocity', leap_velocity)
+        #self.leap_effort = rospy.ServiceProxy('/leap_effort', leap_effort)
         self.pub_hand = rospy.Publisher("/leaphand_node/cmd_ones", JointState, queue_size = 3) 
         r = rospy.Rate(30)   #30hz rate
         while not rospy.is_shutdown():
             r.sleep()
+            ###only run these services if you need them.
             curr_pos = np.array(self.leap_position().position)
+            #curr_vel = np.array(self.leap_velocity().velocity)
+            #curr_eff = np.array(self.leap_effort().effort)
             print(curr_pos)
             ###This is a fresh position, now do some policy stuff here etc.
             
